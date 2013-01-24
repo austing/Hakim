0eb8eeb878a081b73774adf6e837950f2ff45241
Austin <austing@gmail.com>
2013-01-24 14:42:17 +0100

    wsgi

--- hakim/hakim/wsgi.py
+++ hakim/hakim/wsgi.py
@@ -1,28 +1,20 @@
-"""
-WSGI config for hakim project.
+import os, sys, site
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+# Tell wsgi to add the Python site-packages to its path. 
+site.addsitedir('/home/username/.virtualenvs/hakim/lib/python2.7/site-packages')
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hakim.settings'
 
-"""
-import os
+activate_this = os.path.expanduser("~/.virtualenvs/hakim/bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hakim.settings")
+# Calculate the path based on the location of the WSGI script
+project = '/home/mark97229/webapps/hakim/Hakim/hakim'
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
